f5e86363ea52d4456a5af1b05faa22d9c7024d3d
John Marty <JohnRmarty@gmail.com>
2015-02-11 14:01:58 -0700

    finish rspec location caybara

--- spec/features/location_spec.rb
+++ spec/features/location_spec.rb
@@ -26,17 +26,72 @@ describe 'User can CRUD locations' do
   end
 
   scenario 'User can view a show page for a location' do
-    #fill in
+    
+    visit '/'
+
+    click_on "New Location"
+
+    #filling out form for location
+    fill_in 'location[name]', :with => "boston"
+    fill_in 'location[address]', with: "New Address"
+    fill_in 'location[zipcode]', with: "90210"
+
+    #submitting form to create a location
+    click_button "Create Location"
+    click_on "boston"
+
+    expect(page).to have_content("boston")
 
   end
 
   scenario 'User can edit a location' do
-    #fill in
+
+    visit '/'
+
+    click_on "New Location"
+
+    #filling out form for location
+    fill_in 'location[name]', :with => "boston"
+    fill_in 'location[address]', with: "New Address"
+    fill_in 'location[zipcode]', with: "90210"
+
+    #submitting form to create a location
+    click_button "Create Location"
+
+    click_on "Edit"
+
+    fill_in 'location[name]', :with => "denver"
+    fill_in 'location[address]', with: "777 lucky"
+    fill_in 'location[zipcode]', with: "80221"
+
+    click_on "Update Location"
+
+    expect(page).to have_content("denver")
+
+    
   end
 
 
   scenario 'User can delete a location' do
-    #fill in
+
+    visit '/'
+
+    # click on link to go to new location form
+    click_on "New Location"
+
+    #filling out form for location
+    fill_in 'location[name]', :with => "Galvanize"
+    fill_in 'location[address]', with: "New Address"
+    fill_in 'location[zipcode]', with: "90210"
+
+    #submitting form to create a location
+    click_on "Create Location"
+
+    click_on "Delete"
+
+    expect(page).to have_no_content("Galvanize")
+
+
   end
 
 
